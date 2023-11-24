9f5144e5e685f7f2d8b8139afaad75545ab3431f
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-24 09:50:34 +0100

    added templates photo

--- src/main/java/org/course/experis/springilmiofotoalbum/service/PhotoService.java
+++ src/main/java/org/course/experis/springilmiofotoalbum/service/PhotoService.java
@@ -4,65 +4,64 @@ import org.course.experis.springilmiofotoalbum.exceptions.PhotoNotFoundException
 import org.course.experis.springilmiofotoalbum.model.Photo;
 import org.course.experis.springilmiofotoalbum.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PhotoService {
+
     @Autowired
     private PhotoRepository photoRepository;
 
-    public List<Photo> getPhotoList() {
-        return photoRepository.findAll();
-    }
-
-    public List<Photo> getPhotoList(Optional<String> search) {
-
-        if (search.isPresent()) {
-            return
-                    photoRepository.findByTitleContainingIgnoreCase(search.get());
-        } else {
+    //Index
+    public List<Photo> getPhotoList(Optional<String> search){
+        if(search.isPresent()){
+            return photoRepository.findByTitleContainingIgnoreCase(search.get());
+        }
+        else {
             return photoRepository.findAll();
         }
     }
 
+    //Show
     public Photo getPhotoById(Integer id) throws PhotoNotFoundException {
         Optional<Photo> result = photoRepository.findById(id);
-        if (result.isPresent()) {
+        if(result.isPresent()){
             return result.get();
-        } else {
-            throw new PhotoNotFoundException("photo with id " + id + " not found");
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "photo with id " + id + " not found");
         }
     }
 
+    //Create
     public Photo createPhoto(Photo photo) throws PhotoNotFoundException {
-        try {
+        photo.setId(null);
+        try{
             return photoRepository.save(photo);
         } catch (RuntimeException e) {
             throw new PhotoNotFoundException(photo.getTitle());
         }
     }
 
+    //Edit
     public Photo editPhoto(Photo photo) throws PhotoNotFoundException {
         Photo photoToEdit = getPhotoById(photo.getId());
+
         photoToEdit.setTitle(photo.getTitle());
-        photoToEdit.setDescription(photo.getDescription());
-        photoToEdit.setUrl(photo.getUrl());
         photoToEdit.setVisible(photo.isVisible());
+        photoToEdit.setUrl(photo.getUrl());
+        photoToEdit.setDescription(photo.getDescription());
         photoToEdit.setCategories(photo.getCategories());
 
         return photoRepository.save(photoToEdit);
     }
 
-    public void deletePhoto(Integer id) {
+    //Delete
+    public void deletePhoto (Integer id) {
         photoRepository.deleteById(id);
-    }
-
-    public Page<Photo> getPage(Pageable pageable) {
-        return photoRepository.findAll(pageable);
     }
 }
