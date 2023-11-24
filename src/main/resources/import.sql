6ca021574e4d2589e0d95de3ad67059f86f04ca2
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-24 15:32:37 +0100

    values set false

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,7 @@ INSERT INTO roles (id, name) VALUES(1, 'ADMIN');
 INSERT INTO roles (id, name) VALUES(2, 'USER');
 
 INSERT INTO users (email, first_name, last_name, registered_at, password) VALUES('antonio@email.com', 'Antonio', 'Rossi', '2023-11-23 09:00', '{noop}antonio');
-INSERT INTO users (email, first_name, last_name, registered_at, password) VALUES('valeria@email.com', 'Valeria', 'Bianchi', '2023-11-20 16:20','{noop}jane');
+INSERT INTO users (email, first_name, last_name, registered_at, password) VALUES('valeria@email.com', 'Valeria', 'Bianchi', '2023-11-20 16:20','{noop}valeria');
 
 INSERT INTO users_roles (user_id, roles_id) VALUES(1, 1);
 INSERT INTO users_roles (user_id, roles_id) VALUES(1, 2);
@@ -14,7 +14,7 @@ INSERT INTO photos (title, description, url, visible) VALUES ('Casa sul Mare', '
 INSERT INTO photos (title, description, url, visible) VALUES ('Peluche', 'Giocattolo per bambini', 'https://m.media-amazon.com/images/I/61+qD14eLXL._AC_UF1000,1000_QL80_.jpg', true);
 INSERT INTO photos (title, description, url, visible) VALUES ('Personal Computer', 'Apparecchio per lavoro e svago','https://www.pcspecialist.it/images/landing/pcs/gaming-pc/bundle.jpg', true);
 INSERT INTO photos (title, description, url, visible) VALUES ('Bibita', 'Energy drink', 'https://www.drinkshoponline.com/_upload/prodotti/thumb/19205_700_1200_0.jpg', true);
-INSERT INTO photos (title, description, url, visible) VALUES ('La Gioconda', 'Opera conosciuta in tutto il mondo','https://cdn.studenti.stbm.it/images/2017/01/10/gioconda-orig.jpeg', true);
+INSERT INTO photos (title, description, url, visible) VALUES ('La Gioconda', 'Opera conosciuta in tutto il mondo', 'https://cdn.studenti.stbm.it/images/2017/01/10/gioconda-orig.jpeg', true);
 INSERT INTO photos (title, description, url, visible) VALUES ('Mar Mediterraneo', 'Mare che confina con le costeafricane', 'https://ilbolive.unipd.it/sites/default/files/styles/style_1120x630/public/2021-01/mar-mediterraneo-costa.jpg?itok=zX1jZpYJ', true);
 
 INSERT INTO categories(name) VALUES('Città');
